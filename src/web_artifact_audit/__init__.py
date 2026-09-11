82fb568b17a2711000372b72a8b9c7aff26fed19
James Childs-Maidment <10917363+thisbejim@users.noreply.github.com>
2026-09-12 02:33:11 +1000

    docs: expose reference model in package API

--- src/web_artifact_audit/__init__.py
+++ src/web_artifact_audit/__init__.py
@@ -1,7 +1,7 @@
 """Offline static web build-artifact checks."""
 
-from .models import Finding, ScanOptions, ScanResult
+from .models import AssetReference, Finding, ScanOptions, ScanResult
 from .scanner import scan
 
-__all__ = ["Finding", "ScanOptions", "ScanResult", "scan"]
+__all__ = ["AssetReference", "Finding", "ScanOptions", "ScanResult", "scan"]
 __version__ = "0.1.0"
